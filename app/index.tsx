30eeae72c99a2981d33832e7c56cf5b521f191c2
Koncz Gábor <gabor.koncz@ashszoftverhaz.hu>
2024-12-04 15:36:01 +0100

    added nativewind and tailwind with working cofirgurations

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Aora</Text>
+      <StatusBar style="auto" />
+      <Link className="text-sky-400" href="/profile">
+        Go to Profiles
+      </Link>
+    </View>
+  );
+}
